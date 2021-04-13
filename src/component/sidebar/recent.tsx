877f9f1d0ee1d478032cbd8cd07d671ef62299ec
Adel Ameri <a.ameri@live.com>
2021-04-14 00:19:30 +0430

    create recent tsx

---
+++ src/component/sidebar/recent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Recent = () =>{
+    return(
+        <React.Fragment>
+            
+            <div className="mt-3 ltd-font sidebar-obj" style={{right: "10px"}}>
+                <ul className="list-group ltd-0radius p-0">
+                    <li data-href="#" className="list-group-item list-group-item-action text-center">صفحات به تازگی بازدید شده</li>	
+                </ul> 
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Recent
--- src/component/sidebar/sidebar.tsx
+++ src/component/sidebar/sidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactComment from '../../helper/comment'
 import Menues from './menues'
+import Recent from './recent'
 
 const Sidebar = (props : any) =>{
     return(
@@ -20,11 +21,7 @@ const Sidebar = (props : any) =>{
 
                         <ReactComment text='begin recently' />
 
-                        <div className="mt-3 ltd-font sidebar-obj" style={{right: "10px"}}>
-                            <ul className="list-group ltd-0radius p-0">
-                                <li data-href="#" className="list-group-item list-group-item-action text-center">صفحات به تازگی بازدید شده</li>	
-                            </ul> 
-                        </div>
+                        <Recent />                        
 
                         <ReactComment text='end recently' />
 
